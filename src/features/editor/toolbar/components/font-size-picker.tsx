2506bc8c334ad35ebbc73fbef778263999739518
Thien <ubw1212@gmail.com>
2024-08-01 01:24:38 +0700

    Finished font size picker.

--- src/features/editor/toolbar/components/font-size-picker.tsx
+++ src/features/editor/toolbar/components/font-size-picker.tsx
@@ -1,90 +1,67 @@
-import {useState} from "react";
-import {Editor} from "@/features/editor/sidebar/types";
+import { useState } from "react";
+import { Editor } from "@/features/editor/sidebar/types";
+import { FaCheck } from "react-icons/fa6";
 
 interface FontSizePickerProps {
     editor: Editor | undefined;
 }
 
-const FontSizePicker = ({editor} : FontSizePickerProps) => {
+const FontSizePicker = ({ editor }: FontSizePickerProps) => {
+    const [isOpened, setOpened] = useState(false);
 
-    const [isOpened, setOpened] = useState(false)
+    const fontSizes = [6, 8, 10, 12, 14, 16, 18, 21, 24, 28, 32, 36, 42, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120];
 
-    const items = [
-        {
-            key: "new",
-            label: "New file",
-        },
-        {
-            key: "copy",
-            label: "Copy link",
-        },
-        {
-            key: "edit",
-            label: "Edit file",
-        },
-        {
-            key: "delete",
-            label: "Delete file",
-        }
-    ];
-
-    const handleClickPicker = (size : number) => {
-        editor?.changeFontSize(size)
-    }
+    const handleClickPicker = (size: number) => {
+        editor?.changeFontSize(size);
+        setOpened(!isOpened);
+    };
 
     const incrementFontSize = () => {
-        editor?.changeFontSize(editor?.fontSize + 1)
-    }
+        editor?.changeFontSize((editor?.fontSize || 0) + 1);
+    };
 
     const decreaseFontSize = () => {
-        editor?.changeFontSize(editor?.fontSize - 1)
-    }
+        editor?.changeFontSize((editor?.fontSize || 0) - 1);
+    };
 
-    const fontPickerStyle = 'px-4 flex hover:bg-[#f2f3f5] transition-all duration-100 ease-linear h-[40px] w-full items-center'
+    const fontPickerStyle = 'px-4 flex hover:bg-[#f2f3f5] transition-all duration-100 ease-linear h-[40px] w-full items-center';
 
     return (
         <div className='relative'>
             <div className='flex h-[32px] w-[106px]'>
-                <div className='flex flex-1 justify-center items-center cursor-pointer border-y border-l border-gray-400 rounded-l-md
-                         hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
-                     onClick={() => decreaseFontSize()}
-                >-
+                <div
+                    className='flex flex-1 justify-center items-center cursor-pointer border-y border-l border-gray-400 rounded-l-md
+                     hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
+                    onClick={decreaseFontSize}
+                >
+                    -
                 </div>
-                <div className='flex flex-grow justify-center items-center border-x cursor-pointer border-y border-gray-400
-                         hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
-                        onClick={() => setOpened(!isOpened)}
+                <div
+                    className='flex flex-grow justify-center items-center border-x cursor-pointer border-y border-gray-400
+                     hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
+                    onClick={() => setOpened(!isOpened)}
                 >
                     {editor?.fontSize}
                 </div>
-                <div className='flex flex-1 justify-center items-center cursor-pointer border-y border-r border-gray-400 rounded-r-md
-                         hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
-                onClick={() => incrementFontSize()}
-                >+
+                <div
+                    className='flex flex-1 justify-center items-center cursor-pointer border-y border-r border-gray-400 rounded-r-md
+                     hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
+                    onClick={incrementFontSize}
+                >
+                    +
                 </div>
             </div>
-            {isOpened && <div
-                className='fixed flex flex-col py-1 top-[104px] h-[760px] w-[106px] border rounded-2xl bg-white z-50 shadow-xl'>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(6)}}><span>6</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(8)}}><span>8</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(10)}}><span>10</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(12)}}><span>12</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(14)}}><span>14</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(16)}}><span>16</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(18)}}><span>18</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(21)}}><span>21</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(24)}}><span>24</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(28)}}><span>28</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(32)}}><span>32</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(36)}}><span>36</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(42)}}><span>42</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(48)}}><span>48</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(56)}}><span>56</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(64)}}><span>64</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(72)}}><span>72</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(80)}}><span>80</span></div>
-                <div className={fontPickerStyle} onClick={() => {handleClickPicker(88)}}><span>88</span></div>
-
-            </div>}
+            {isOpened && (
+                <div className='fixed flex flex-col py-1 top-[104px] h-[840px] w-[106px] border rounded-2xl bg-white z-50 shadow-xl'>
+                    {fontSizes.map(size => (
+                        <div key={size} className={fontPickerStyle} onClick={() => handleClickPicker(size)}>
+              <span className='flex w-full items-center justify-between'>
+                {size} {editor?.fontSize === size && <FaCheck />}
+              </span>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
