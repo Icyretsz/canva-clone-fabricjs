c637284c120326c5878ea4d3679133a1b94baff8
Thien <ubw1212@gmail.com>
2024-08-02 02:13:01 +0700

    Add a dropdown arrow inside the font-picker.

--- src/features/editor/toolbar/components/font-picker.tsx
+++ src/features/editor/toolbar/components/font-picker.tsx
@@ -3,7 +3,7 @@ import {Editor} from "@/features/editor/sidebar/types";
 import useMenuStore from "@/features/editor/stores/store";
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
 import {textboxFonts} from "@/features/editor/utils";
-
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 interface FontPickerProps {
     editor: Editor | undefined;
@@ -31,13 +31,13 @@ const FontPicker = ({editor}: FontPickerProps) => {
             <TooltipProvider>
                 <Tooltip delayDuration={200}>
                     <TooltipTrigger asChild>
-                        <div className='w-[170px] h-[32px] flex justify-center items-center cursor-pointer border
+                        <div className='w-[170px] h-[32px] pl-4 pr-1 flex justify-between items-center cursor-pointer border
         border-gray-400 rounded-md hover:bg-[#f2f3f5] transition-all duration-100 ease-linear'
                              onClick={() => {
                                  setActiveTool('Font')
                                  setExpanded(true)
                              }}>
-                            {fontName}
+                            {fontName} <RiArrowDropDownLine className='size-7'/>
                         </div>
                     </TooltipTrigger>
                     <TooltipContent side="bottom" sideOffset={10}>
