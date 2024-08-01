b6e839576ab7deee06d8a154c85ebfbd71540420
Thien <ubw1212@gmail.com>
2024-08-02 00:02:55 +0700

    Simplifying font-menu.tsx: creating component to render a AccordionItem for each font.

--- src/features/editor/sidebar/components/font-menu.tsx
+++ src/features/editor/sidebar/components/font-menu.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/accordion"
 import {FaCheck} from "react-icons/fa6";
 import {textboxFonts} from "@/features/editor/utils";
+import {NextFont} from "next/dist/compiled/@next/font";
 
 interface FontMenuProps {
     editor: Editor | undefined;
@@ -58,7 +59,7 @@ const FontMenu = ({editor}: FontMenuProps) => {
         editor?.changeFontFamily(fontFamily);
     }
 
-    const fontCheck = (Font : string): boolean => {
+    const fontCheck = (Font: string): boolean => {
         const fontsArray = textboxFonts(editor)
         if (fontsArray.has(Font)) {
             return true
@@ -66,6 +67,69 @@ const FontMenu = ({editor}: FontMenuProps) => {
         return false
     }
 
+    interface RenderAccordionItemProps {
+        fontFamily: string;
+        light: any;
+        normal: any;
+        bold: any;
+        fontCheck: (font: string) => boolean;
+        handleClick: (fontFamily: string) => void;
+    }
+
+
+    const RenderAccordionItem = ({
+                                     fontFamily,
+                                     light,
+                                     normal,
+                                     bold,
+                                     fontCheck,
+                                     handleClick
+                                 }: RenderAccordionItemProps) => {
+
+        return (
+            <>
+                <AccordionItem value={fontFamily}>
+                    <AccordionTrigger>
+                                <span
+                                    className={`${normal.className} flex pr-2 w-full items-center justify-between text-black text-[28px]`}>
+                                    {fontFamily} {(fontCheck(light.style.fontFamily) ||
+                                    fontCheck(normal.style.fontFamily) ||
+                                    fontCheck(bold.style.fontFamily)) && <FaCheck/>}</span>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                        <div className="h-full w-full flex flex-col">
+                            <div
+                                className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
+                                onClick={() => handleClick(light.style.fontFamily)}
+                            >
+                                        <span
+                                            className={`${light.className} flex pr-2 w-full items-center justify-between pl-4 font-light text-black text-[24px] `}>
+                                            Light {fontCheck(light.style.fontFamily) && <FaCheck/>}</span>
+                            </div>
+
+                            <div
+                                className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
+                                onClick={() => handleClick(normal.style.fontFamily)}
+                            >
+                                        <span
+                                            className={`${normal.className} flex pr-2 w-full items-center justify-between pl-4 font-normal text-black text-[24px] `}>
+                                            Normal {fontCheck(normal.style.fontFamily) && <FaCheck/>}</span>
+                            </div>
+
+                            <div
+                                className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
+                                onClick={() => handleClick(bold.style.fontFamily)}
+                            >
+                                        <span
+                                            className={`${bold.className} flex pr-2 w-full items-center justify-between pl-4 font-bold text-black text-[24px] `}>
+                                            Bold {fontCheck(bold.style.fontFamily) && <FaCheck/>}</span>
+                            </div>
+                        </div>
+                    </AccordionContent>
+                </AccordionItem>
+            </>
+        )
+    }
 
 
     return (
@@ -74,122 +138,12 @@ const FontMenu = ({editor}: FontMenuProps) => {
                 <MenuHeaderLight type={'Fonts'}/>
                 <div className="h-full w-full flex flex-col items-center gap-5">
                     <Accordion type="multiple" className='h-[40px] flex flex-col px-4 w-full'>
-                        <AccordionItem value="montserrat">
-                            <AccordionTrigger>
-                                <span
-                                    className={`${montserratNormal.className} flex pr-2 w-full items-center justify-between text-black text-[28px]`}>
-                                    Montserrat {(fontCheck(montserratLight.style.fontFamily) ||
-                                    fontCheck(montserratNormal.style.fontFamily) ||
-                                    fontCheck(montserratBold.style.fontFamily)) && <FaCheck/>}</span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <div className="h-full w-full flex flex-col">
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(montserratLight.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${montserratLight.className} flex pr-2 w-full items-center justify-between pl-4 font-light text-black text-[24px] `}>
-                                            Light {fontCheck(montserratLight.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(montserratNormal.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${montserratNormal.className} flex pr-2 w-full items-center justify-between pl-4 font-normal text-black text-[24px] `}>
-                                            Normal {fontCheck(montserratNormal.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(montserratBold.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${montserratBold.className} flex pr-2 w-full items-center justify-between pl-4 font-bold text-black text-[24px] `}>
-                                            Bold {fontCheck(montserratBold.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-                                </div>
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="roboto">
-                            <AccordionTrigger>
-                                <span className={`${robotoNormal.className} flex pr-2 w-full items-center justify-between text-black text-[28px]`}>
-                                    Roboto {(fontCheck(robotoLight.style.fontFamily) ||
-                                    fontCheck(robotoNormal.style.fontFamily) ||
-                                    fontCheck(robotoBold.style.fontFamily)) && <FaCheck/>}</span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <div className="h-full w-full flex flex-col">
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(robotoLight.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${robotoLight.className} flex pr-2 w-full items-center justify-between pl-4 font-light text-black text-[24px] `}>
-                                            Light {fontCheck(robotoLight.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(robotoNormal.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${robotoNormal.className} flex pr-2 w-full items-center justify-between pl-4 font-normal text-black text-[24px] `}>
-                                            Normal {fontCheck(robotoNormal.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(robotoBold.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${robotoBold.className} flex pr-2 w-full items-center justify-between pl-4 font-bold text-black text-[24px] `}>
-                                            Bold {fontCheck(robotoBold.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-                                </div>
-                            </AccordionContent>
-                        </AccordionItem>
-                        <AccordionItem value="bellota">
-                            <AccordionTrigger>
-                                <span
-                                    className={`${bellotaNormal.className} flex pr-2 w-full items-center justify-between text-black text-[28px]`}>
-                                    Bellota {(fontCheck(bellotaLight.style.fontFamily) ||
-                                    fontCheck(bellotaNormal.style.fontFamily) ||
-                                    fontCheck(bellotaBold.style.fontFamily))  && <FaCheck/>}</span>
-                            </AccordionTrigger>
-                            <AccordionContent>
-                                <div className="h-full w-full flex flex-col">
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(bellotaLight.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${bellotaLight.className} flex pr-2 w-full items-center justify-between pl-4 font-light text-black text-[24px] `}>
-                                            Light {fontCheck(bellotaLight.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(bellotaNormal.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${bellotaNormal.className} flex pr-2 w-full items-center justify-between pl-4 font-normal text-black text-[24px] `}>
-                                            Normal {fontCheck(bellotaNormal.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-
-                                    <div
-                                        className='hover:bg-[#f2f3f5] transition-all duration-100 ease-linear w-full h-[40px] flex items-center'
-                                        onClick={() => handleClick(bellotaBold.style.fontFamily)}
-                                    >
-                                        <span
-                                            className={`${bellotaBold.className} flex pr-2 w-full items-center justify-between pl-4 font-bold text-black text-[24px] `}>
-                                            Bold {fontCheck(bellotaBold.style.fontFamily) && <FaCheck/>}</span>
-                                    </div>
-                                </div>
-                            </AccordionContent>
-                        </AccordionItem>
+                        <RenderAccordionItem fontFamily={'Montserrat'} light={montserratLight} normal={montserratNormal}
+                                             bold={montserratBold} fontCheck={fontCheck} handleClick={handleClick}/>
+                        <RenderAccordionItem fontFamily={'Roboto'} light={robotoLight} normal={robotoNormal}
+                                             bold={robotoBold} fontCheck={fontCheck} handleClick={handleClick}/>
+                        <RenderAccordionItem fontFamily={'Bellota'} light={bellotaLight} normal={bellotaNormal}
+                                             bold={bellotaBold} fontCheck={fontCheck} handleClick={handleClick}/>
                     </Accordion>
 
                 </div>
