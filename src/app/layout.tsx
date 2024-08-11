0eb93191c7454a18ecdd38a3ab2db479e7ab4096
Thien <ubw1212@gmail.com>
2024-08-11 11:58:13 +0700

    Add sign-in and sign-out custom pages. Add user button to header.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={nunito.className}>
       <NextUIProvider>
-          <ClerkProvider>
+          <ClerkProvider >
       {children}
           </ClerkProvider>
       </NextUIProvider>
--- src/features/editor/components/header.tsx
+++ src/features/editor/components/header.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+'use client'
+import React, {useEffect, useState} from 'react';
+import {UserButton} from '@clerk/nextjs'
 
 const Header = () => {
+    const [rendered, setRendered] = useState(false)
+    useEffect(() => {
+        setRendered(true)
+    }, [])
+
     return (
-        <div className=' w-full h-[68px] text-white absolute px-4 flex items-center text-3xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...'>
+        <div className=' w-full h-[68px] text-white absolute px-4 flex items-center justify-between text-3xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...'>
             Canza
+            { rendered && <UserButton/>}
         </div>
     );
 };
