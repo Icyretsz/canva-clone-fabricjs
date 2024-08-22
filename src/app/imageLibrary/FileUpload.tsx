df386588d5b69362348d52fe8b8d49448fe1d3cb
IcyretszMacbookPro16 <ubw1212@gmail.com>
2024-08-22 22:03:51 +0700

    Allow uploading multiple images at once.

--- src/app/imageLibrary/FileUpload.tsx
+++ src/app/imageLibrary/FileUpload.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, {useState, ChangeEvent, FormEvent} from 'react';
-import  Image  from 'next/image'
+import Image from 'next/image'
 // @ts-ignore
 import {v4} from 'uuid';
-import { useUser } from '@clerk/nextjs';
+import {useUser} from '@clerk/nextjs';
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import addMedia from "@/app/db/addMedia";
 
@@ -11,15 +11,15 @@ const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
 
 const FileUpload: React.FC = () => {
     const queryClient = useQueryClient()
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [selectedFiles, setSelectedFiles] = useState<File[] | null>([]);
     const [uploading, setUploading] = useState(false)
-    const [fileURL, setFileURL] = useState('')
-    const { user } = useUser();
+    const [fileURLs, setFileURLs] = useState<string[]>([])
+    const {user} = useUser();
 
     const mutation = useMutation({
         mutationFn: addMedia,
         onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['media'] })
+            queryClient.invalidateQueries({queryKey: ['media']})
         },
     })
 
@@ -27,15 +27,16 @@ const FileUpload: React.FC = () => {
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files.length > 0) {
             const fileList = event.target.files;
-            for (let i = 0; i < fileList.length; i++) {
-                const file = fileList[i];
+            const fileArray = Array.from(fileList)
+            for (let i = 0; i < fileArray.length; i++) {
+                const file = fileArray[i];
                 if (!allowedTypes.includes(file.type)) {
                     alert('Invalid file type. Please upload a JPEG, PNG, or GIF image.');
                     event.target.value = ''
                     return;
                 }
             }
-            setSelectedFile(event.target.files[0]);
+            setSelectedFiles(fileArray);
         }
     };
 
@@ -45,64 +46,69 @@ const FileUpload: React.FC = () => {
             console.error('File upload is in process, please wait')
             return;
         }
-        if (!selectedFile) {
+        if (!selectedFiles) {
             console.error('No file selected');
             return;
         }
 
         setUploading(true);
-        const fileName = `${v4()}-${selectedFile.name}`
+        for (const selectedFile of selectedFiles) {
 
-        try {
-            const PUTURLResponse = await fetch('/api/upload/put', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': selectedFile.type,
-                },
-                body: JSON.stringify({ fileName: fileName }),
-            });
+            const fileName = `${v4()}-${selectedFile.name}`
 
-            const PUTSignedURL = await PUTURLResponse.json();
-            if (PUTURLResponse.ok && PUTSignedURL.url) {
-                const uploadResponse = await fetch(PUTSignedURL.url, {
-                    method: 'PUT',
+            try {
+                const PUTURLResponse = await fetch('/api/upload/put', {
+                    method: 'POST',
                     headers: {
                         'Content-Type': selectedFile.type,
                     },
-                    body: selectedFile,
+                    body: JSON.stringify({fileName: fileName}),
                 });
 
-                if (uploadResponse.ok) {
-                    mutation.mutate({
-                        user_id: user!.id,
-                        url: fileName,
-                    })
-
-                    const GETURLResponse = await fetch(`/api/upload/get?fileName=${fileName}`, {
-                        method: 'GET',
+                const PUTSignedURL = await PUTURLResponse.json();
+                if (PUTURLResponse.ok && PUTSignedURL.url) {
+                    const uploadResponse = await fetch(PUTSignedURL.url, {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': selectedFile.type,
+                        },
+                        body: selectedFile,
                     });
-                    const GETResponseURL = await GETURLResponse.json();
-                    if (GETURLResponse.ok && GETResponseURL.url) {
-                        setFileURL(GETResponseURL.url);
+
+                    if (uploadResponse.ok) {
+                        mutation.mutate({
+                            user_id: user!.id,
+                            url: fileName,
+                        })
+
+                        const GETURLResponse = await fetch(`/api/upload/get?fileName=${fileName}`, {
+                            method: 'GET',
+                        });
+                        const GETResponseURL = await GETURLResponse.json();
+                        if (GETURLResponse.ok && GETResponseURL.url) {
+                            setFileURLs((prevURLs) => [
+                                ...prevURLs,
+                                GETResponseURL.url
+                            ]);
+                        } else {
+                            console.error('Failed to retrieve signed URL:', GETResponseURL.error || GETURLResponse.statusText);
+                        }
+                        // console.log('File uploaded successfully');
+                        // const s3Region = process.env.NEXT_PUBLIC_S3_BUCKET_REGION as string;
+                        // const s3BucketName = process.env.NEXT_PUBLIC_S3_BUCKET as string;
+                        // const url = `https://${s3BucketName}.s3.${s3Region}.amazonaws.com/${fileName}`
+                        // setFileURL(url)
                     } else {
-                        console.error('Failed to retrieve signed URL:', GETResponseURL.error || GETURLResponse.statusText);
+                        console.error('File upload failed');
                     }
-                    // console.log('File uploaded successfully');
-                    // const s3Region = process.env.NEXT_PUBLIC_S3_BUCKET_REGION as string;
-                    // const s3BucketName = process.env.NEXT_PUBLIC_S3_BUCKET as string;
-                    // const url = `https://${s3BucketName}.s3.${s3Region}.amazonaws.com/${fileName}`
-                    // setFileURL(url)
                 } else {
-                    console.error('File upload failed');
+                    console.error('Failed to get upload URL:', PUTSignedURL.error);
                 }
-            } else {
-                console.error('Failed to get upload URL:', PUTSignedURL.error);
+            } catch (error) {
+                console.error('An error occurred during the upload:', error);
             }
-        } catch (error) {
-            console.error('An error occurred during the upload:', error);
-        } finally {
-            setUploading(false);
         }
+        setUploading(false)
     };
 
     return (
@@ -110,15 +116,18 @@ const FileUpload: React.FC = () => {
             <h1>Upload a File</h1>
             <form onSubmit={handleFileUpload}>
                 <div>
-                    <input type="file" onChange={handleFileChange} />
+                    <input type="file" multiple onChange={handleFileChange}/>
                 </div>
                 <div>
                     <button className="bg-blue-200 text-black border border-black" type="submit">Upload</button>
                 </div>
             </form>
-            {selectedFile && <p>Selected file: {selectedFile.name}</p>}
-            {uploading && <div>Uploading...</div> }
-            {fileURL !== "" && <Image src={fileURL} width={400} height={400} alt='uploaded image'/>}
+            {uploading && <div>Uploading...</div>}
+            {fileURLs.length !== 0 &&
+                fileURLs.map((fileURL, i) => {
+                    return <Image key={i} src={fileURL} width={400} height={400} alt='uploaded image'/>
+                })
+            }
         </div>
     );
 };
